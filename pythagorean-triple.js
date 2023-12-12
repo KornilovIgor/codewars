3f6fbb82958b7fc59963a5af112a0ef7cce42a95
KornilovIgor <mail@kote.dev>
2023-12-12 23:52:01 +0300

    "Pythagorean Triple" is done.

---
+++ pythagorean-triple.js
@@ -0,0 +1,22 @@
+// ❓DESCRIPTION:
+/*
+Set the playerNumber variable to 16. Then, use the variable to look up the player's
+name and assign it to player.
+
+// Setup
+const testObj = {
+  12: "Namath",
+  16: "Montana",
+  19: "Unitas"
+};
+
+// Only change code below this line
+const playerNumber = 42;  // Change this line
+const player = testObj;   // Change this line
+*/
+
+// ✅SOLUTION:
+function isPythagoreanTriple(integers)
+{
+  return (integers[0] * integers[0] + integers[1] * integers[1] == integers[2] * integers[2]);
+}
