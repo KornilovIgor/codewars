e09b5f38bfed0c2391db29633ad7a1319cd6b8ca
KornilovIgor <mail@kote.dev>
2023-12-21 00:25:15 +0300

    "Does My List Include This" is done.

---
+++ 8kyu/enumerable-magic-does-my-list-include-this.js
@@ -0,0 +1,7 @@
+// ❓DESCRIPTION:
+/*Enumerable Magic - Does My List Include This?
+
+Create a method that accepts a list and an item, and returns true if the item belongs to the list, otherwise false.*/
+
+// ✅SOLUTION:
+const include = (arr, item) => arr.includes(item);
