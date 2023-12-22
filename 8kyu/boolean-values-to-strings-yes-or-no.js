012fa291cb00813de9163fcec68cbf1ecd569523
KornilovIgor <mail@kote.dev>
2023-12-22 23:55:42 +0300

    "Convert boolean values to strings" is done.

---
+++ 8kyu/boolean-values-to-strings-yes-or-no.js
@@ -0,0 +1,7 @@
+// ❓DESCRIPTION:
+/*Convert boolean values to strings 'Yes' or 'No'.
+
+Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.*/
+
+// ✅SOLUTION:
+const boolToWord = (bool) => bool ? "Yes" : "No";
